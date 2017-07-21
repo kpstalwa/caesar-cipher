aa4a2e8a39554de02e3fbc387c43734a00eb31a3
Karam Talwandi <k.talwandi@hotmail.com>
2017-07-20 20:37:49 -0400

    simplify code

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,31 +1,26 @@
 def caesar_cipher(word, shift)
 	shift = shift.to_i
-	alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	alphabet = ('a'..'z').to_a
 	newWord = ''
 	arrword = word.split(' ') #split into array
 	arrword.each do |uncipheredWord|
 		index=0
 		lengthofWord=uncipheredWord.length
-		#puts uncipheredWord
-		#puts lengthofWord
 		while index < lengthofWord
 			currentChar = uncipheredWord[index]
 			newAlphabet=currentChar
 			currentChar = currentChar.downcase
 			if !alphabet.include?(currentChar)
 				index+=1
-				#puts newAlphabet
 				newWord+=newAlphabet
 				next
 			end
 			alphabetIndex = alphabet.index(currentChar)
-			#puts "alphabet index of the letter #{currentChar} is #{alphabetIndex}"
 			alphabetIndex = alphabetIndex.to_i
 			alphabetIndex+= shift
 			if alphabetIndex>=26
 				alphabetIndex = alphabetIndex%26
 			end
-			#puts "alphabet index after shift of #{currentChar} is #{alphabetIndex}"
 			if currentChar.upcase == newAlphabet
 				newAlphabet = alphabet[alphabetIndex]
 				newAlphabet = newAlphabet.upcase
@@ -34,18 +29,9 @@ def caesar_cipher(word, shift)
 				newAlphabet = alphabet[alphabetIndex]
 			end
 			newWord+=newAlphabet
-			#puts newAlphabet
 			index+=1
 		end
 		newWord+=' '
 	end
 	newWord
 end
-
-=begin
-puts "Please enter what you wish to encrypt"
-word = gets.chomp
-puts "Enter the desired shift"
-shift = gets.chomp
-caesar_cipher(word, shift)
-=end
